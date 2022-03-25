36c5e3b04cc2ab689fb9cd73d5a52292bea1c6d6
Alexey Kiselev <ximand931@gmail.com>
2022-03-25 13:45:50 +0300

    module and view model fix, nav graph tools

--- app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
@@ -2,6 +2,7 @@ package com.team.seven.gocomix.ui.home
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel : ViewModel()
+class HomeViewModel @Inject constructor() : ViewModel()
