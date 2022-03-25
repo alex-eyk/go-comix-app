093eb0f5c46c56c5ee85f8d7e25a0572b040817b
Alexey Kiselev <ximand931@gmail.com>
2022-03-25 13:14:10 +0300

    ui package, home hilt view model

--- app/src/main/java/com/team/seven/gocomix/ui/home/HomeFragment.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/home/HomeFragment.kt
@@ -1,14 +1,18 @@
-package com.team.seven.gocomix
+package com.team.seven.gocomix.ui.home
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.team.seven.gocomix.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
---
+++ app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
@@ -0,0 +1,7 @@
+package com.team.seven.gocomix.ui.home
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class HomeViewModel : ViewModel()
