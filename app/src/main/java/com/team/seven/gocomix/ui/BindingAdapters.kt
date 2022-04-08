f955ac68ca44f7ee787aff85744240e51354d5ac
Alexey Kiselev <ximand931@gmail.com>
2022-04-08 19:38:57 +0300

    comix info xml

---
+++ app/src/main/java/com/team/seven/gocomix/ui/BindingAdapters.kt
@@ -0,0 +1,13 @@
+package com.team.seven.gocomix.ui
+
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+
+object BindingAdapters {
+
+    @JvmStatic
+    @BindingAdapter("clipToOutline")
+    fun setClipToOutline(imageView: ImageView, clip: Boolean) {
+        imageView.clipToOutline = clip
+    }
+}
