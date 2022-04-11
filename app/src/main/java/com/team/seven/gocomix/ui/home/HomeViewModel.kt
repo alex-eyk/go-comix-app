313f0a8ca2046efa0a3a270272ee9280d13fcc2d
Alexey Kiselev <ximand931@gmail.com>
2022-04-11 21:56:37 +0300

    comix repository di

--- app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
@@ -1,8 +1,11 @@
 package com.team.seven.gocomix.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.team.seven.gocomix.repo.ComixRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel()
+class HomeViewModel @Inject constructor(
+    private val comixRepository: ComixRepository
+) : ViewModel()
