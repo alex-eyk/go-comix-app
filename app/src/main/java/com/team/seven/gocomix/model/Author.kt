34e6bd84f3ea7ecf9a6292dbca579556fa3060c7
Alexey Kiselev <44301479+alex-eyk@users.noreply.github.com>
2022-04-12 21:47:00 +0300

    Revert "Gradle, strict mode"

--- app/src/main/java/com/team/seven/gocomix/model/Author.kt
+++
@@ -1,7 +0,0 @@
-package com.team.seven.gocomix.model
-
-data class Author(
-    val email: String,
-    val id: Int,
-    val login: String
-)
--- app/src/main/java/com/team/seven/gocomix/model/Page.kt
+++
@@ -1,8 +0,0 @@
-package com.team.seven.gocomix.model
-
-data class Page(
-    val comixId: Int,
-    val id: Int,
-    val image: String,
-    val number: Int
-)
