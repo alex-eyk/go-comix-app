f8c80924a16767fbee8bcc8459cd6db92d45f654
paveldolgov4 <dolgoff1272@gmail.com>
2022-04-18 14:53:23 +0300

    hide bottom nav

--- app/src/main/java/com/team/seven/gocomix/ui/login/SignInFragment.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/login/SignInFragment.kt
@@ -1,6 +1,8 @@
 package com.team.seven.gocomix.ui.login
 
+import android.view.View
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.team.seven.gocomix.R
 import com.team.seven.gocomix.databinding.FragmentSignInBinding
 import com.team.seven.gocomix.ui.AbstractFragment
@@ -19,6 +21,17 @@ class SignInFragment : AbstractFragment<FragmentSignInBinding, SignInViewModel>(
 
     override fun onBindingCreated() {
         super.binding.loginLoginButton.setOnClickListener {
+            navController.navigate(R.id.action_navigation_sign_in_to_navigation_home)
         }
+        val navBar: BottomNavigationView = requireActivity()
+            .findViewById(R.id.bottom_navigation_view)
+        navBar.visibility = View.GONE
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        val navBar: BottomNavigationView = requireActivity()
+            .findViewById(R.id.bottom_navigation_view)
+        navBar.visibility = View.VISIBLE
     }
 }
