aed62cd6035d3950b03dd4bf7b60a36d0a649a81
kirktriplefive <kirkplat@gmail.com>
2022-04-18 16:18:25 +0300

    starting add signup+google sign in

--- app/src/main/java/com/team/seven/gocomix/ui/signin/SignInViewModel.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/signin/SignInViewModel.kt
@@ -1,8 +1,24 @@
 package com.team.seven.gocomix.ui.signin
 
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.team.seven.gocomix.ui.splash.AuthState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class SignInViewModel @Inject constructor() : ViewModel()
+class SignInViewModel @Inject constructor(
+    private val firebaseAuth: FirebaseAuth
+) : ViewModel() {
+
+    private val _userState: MutableStateFlow<AuthState> = MutableStateFlow(AuthState.Loading)
+
+    val userState: StateFlow<AuthState> = _userState
+
+    fun updateAuthState() {
+        _userState.value = AuthState.by(firebaseAuth.currentUser)
+    }
+}
+
--- app/src/main/java/com/team/seven/gocomix/ui/signup/SignUpFragment.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/signup/SignUpFragment.kt
@@ -1,6 +1,7 @@
 package com.team.seven.gocomix.ui.signup
 
 import androidx.fragment.app.viewModels
+import com.google.android.material.textfield.TextInputEditText
 import com.team.seven.gocomix.R
 import com.team.seven.gocomix.databinding.FragmentSignUpBinding
 import com.team.seven.gocomix.ui.AbstractFragment
@@ -10,12 +11,39 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignUpFragment : AbstractFragment<FragmentSignUpBinding, SignUpViewModel>(
     layoutRes = R.layout.fragment_sign_up
 ) {
+    lateinit var editText: TextInputEditText
+    lateinit var username: String
+    lateinit var email: String
+    private lateinit var password: String
+    private lateinit var pasw: String
+
 
     companion object {
         private const val TAG = "SignUpFragment"
     }
 
     override val viewModel: SignUpViewModel by viewModels()
+
+    override fun onBindingCreated() {
+        super.binding.registerUsernameEditText.setOnClickListener {
+            username = editText.text.toString()
+        }
+        super.binding.registerEmailEditText.setOnClickListener {
+            email = editText.text.toString()
+        }
+        super.binding.registerPasswordEditText.setOnClickListener {
+            password = editText.text.toString()
+        }
+        super.binding.loginPasswordAgainEditText.setOnClickListener {
+            pasw = editText.text.toString()
+        }
+        super.binding.btnRegister.setOnClickListener {
+
+
+        }
+
+    }
+
 }
 
 
