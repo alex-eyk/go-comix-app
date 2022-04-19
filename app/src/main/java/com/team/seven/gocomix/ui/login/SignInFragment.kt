bf463fad5c419e51c2356ff659061ba165b108d3
paveldolgov4 <dolgoff1272@gmail.com>
2022-04-19 17:53:09 +0300

    fragment navBar init

--- app/src/main/java/com/team/seven/gocomix/ui/login/SignInFragment.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/login/SignInFragment.kt
@@ -12,7 +12,10 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignInFragment : AbstractFragment<FragmentSignInBinding, SignInViewModel>(
     layoutRes = R.layout.fragment_sign_in
 ) {
-
+    private val navBar: BottomNavigationView by lazy {
+        requireActivity().findViewById(R.id.bottom_navigation_view)
+    }
+    // инициализация произойдет при первом обращении к navBar
     companion object {
         private const val TAG = "SignInFragment"
     }
@@ -23,15 +26,11 @@ class SignInFragment : AbstractFragment<FragmentSignInBinding, SignInViewModel>(
         super.binding.loginLoginButton.setOnClickListener {
             navController.navigate(R.id.action_navigation_sign_in_to_navigation_home)
         }
-        val navBar: BottomNavigationView = requireActivity()
-            .findViewById(R.id.bottom_navigation_view)
         navBar.visibility = View.GONE
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
-        val navBar: BottomNavigationView = requireActivity()
-            .findViewById(R.id.bottom_navigation_view)
         navBar.visibility = View.VISIBLE
     }
 }
