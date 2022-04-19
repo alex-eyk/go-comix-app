bfc027752ec34aea98e230ea71dc275b14b968bb
Alexey Kiselev <ximand931@gmail.com>
2022-04-19 12:18:54 +0300

    home fragment – bottom sheet

---
+++ app/src/main/java/com/team/seven/gocomix/util/BottomSheetBehaivourExt.kt
@@ -0,0 +1,38 @@
+package com.team.seven.gocomix.util
+
+import android.view.View
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
+fun <V : View> BottomSheetBehavior<V>.hide() {
+    this.state = BottomSheetBehavior.STATE_HIDDEN
+}
+
+fun <V : View> BottomSheetBehavior<V>.expand() {
+    this.state = BottomSheetBehavior.STATE_EXPANDED
+}
+
+fun <V : View> BottomSheetBehavior<V>.setOnStateChange(
+    callback: ((sheet: View, state: Int) -> Unit)
+) {
+    this.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+            callback.invoke(bottomSheet, newState)
+        }
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+        }
+    })
+}
+
+fun <V : View> BottomSheetBehavior<V>.setOnSlide(
+    callback: ((sheet: View, slideOffset: Float) -> Unit)
+) {
+    this.addBottomSheetCallback(object : BottomSheetBehavior.BottomSheetCallback() {
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+        }
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            callback.invoke(bottomSheet, slideOffset)
+        }
+    })
+}
