95bf0bf53caf4b4b8d41988fe19a03477de3e0d5
kirktriplefive <kirkplat@gmail.com>
2022-04-19 22:15:03 +0300

    Revert "starting add signup+google sign in"
    This reverts commit aed62cd6

--- app/src/main/java/com/team/seven/gocomix/ui/signin/SignInFragment.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/signin/SignInFragment.kt
@@ -1,14 +1,6 @@
 package com.team.seven.gocomix.ui.signin
 
-import android.app.Activity
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.view.View
 import androidx.fragment.app.viewModels
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseAuth
 import com.team.seven.gocomix.R
 import com.team.seven.gocomix.databinding.FragmentSignInBinding
 import com.team.seven.gocomix.ui.AbstractFragment
@@ -17,54 +9,19 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class SignInFragment : AbstractFragment<FragmentSignInBinding, SignInViewModel>(
     layoutRes = R.layout.fragment_sign_in
-
 ) {
+
     companion object {
         private const val TAG = "SignInFragment"
-        const val SIGN_IN_RESULT_CODE = 1001
     }
 
     override val viewModel: SignInViewModel by viewModels()
 
     override fun onBindingCreated() {
-        super.binding.loginUsingGoogleButton.setOnClickListener {
-            val providers = arrayListOf(
-                AuthUI.IdpConfig.GoogleBuilder().build()
-            )
-
-            startActivityForResult(
-                AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
-                    providers
-                ).build(), SIGN_IN_RESULT_CODE
-            )
+        super.binding.loginLoginButton.setOnClickListener {
         }
         super.binding.loginRegisterButton.setOnClickListener {
             navController.navigate(R.id.navigation_sign_up)
         }
-
     }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == SIGN_IN_RESULT_CODE) {
-            val response = IdpResponse.fromResultIntent(data)
-            if (resultCode == Activity.RESULT_OK) {
-                // Successfully signed in user.
-                Log.i(
-                    TAG,
-                    "Successfully signed in user " +
-                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
-                )
-            } else {
-                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
-            }
-        }
-    }
-
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        viewModel.updateAuthState()
-    }
-
 }
