8f9d79736f6004c51d512fab8a7c7b8e8b11af32
Alexey Kiselev <ximand931@gmail.com>
2022-04-23 11:00:53 +0300

    using di

--- app/src/main/java/com/team/seven/gocomix/di/AppModule.kt
+++ app/src/main/java/com/team/seven/gocomix/di/AppModule.kt
@@ -1,13 +1,17 @@
 package com.team.seven.gocomix.di
 
 import com.google.firebase.auth.FirebaseAuth
+import com.team.seven.gocomix.model.Constants
 import com.team.seven.gocomix.repo.ComixRepository
 import com.team.seven.gocomix.repo.net.ComixNetRepository
+import com.team.seven.gocomix.repo.net.ComixService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -15,8 +19,23 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideComixRepository(): ComixRepository {
-        return ComixNetRepository()
+    fun provideComixService(retrofit: Retrofit): ComixService {
+        return retrofit.create(ComixService::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideComicRepository(comixService: ComixService): ComixRepository {
+        return ComixNetRepository(comixService)
     }
 
     @Singleton
