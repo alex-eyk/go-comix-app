f4d844d8dbb354bf741318f94c4a2c376be7a3b3
paveldolgov4 <dolgoff1272@gmail.com>
2022-05-06 18:04:51 +0300

    home save state

--- app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/team/seven/gocomix/ui/home/HomeViewModel.kt
@@ -1,20 +1,23 @@
 package com.team.seven.gocomix.ui.home
 
+import android.os.Parcelable
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.team.seven.gocomix.model.Comix
 import com.team.seven.gocomix.repo.ComixRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
     private val comixRepository: ComixRepository
 ) : ViewModel() {
+
+    var listState: Parcelable? = null
 
     private val _comicsState: MutableStateFlow<ComicsUiState> =
         MutableStateFlow(ComicsUiState.Loading)
