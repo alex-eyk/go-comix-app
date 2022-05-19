a73db849a5b7bb6aaf6694b63d5aa7cf25129b0f
Alexey Kiselev <ximand931@gmail.com>
2022-05-19 18:45:14 +0300

    image upd

--- app/src/main/java/com/team/seven/gocomix/model/Image.kt
+++ app/src/main/java/com/team/seven/gocomix/model/Image.kt
@@ -1,6 +1,8 @@
 package com.team.seven.gocomix.model
 
+@Deprecated("use API method image/{id} instead")
 data class Image(
+    val id: Long,
     val origin: String,
     val small: String,
     val loading: String
