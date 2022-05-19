64bd3785d055030505e403f0eb0d5701a4def795
Alexey Kiselev <ximand931@gmail.com>
2022-05-19 18:45:14 +0300

    image upd

--- app/src/main/java/com/team/seven/gocomix/model/Comix.kt
+++ app/src/main/java/com/team/seven/gocomix/model/Comix.kt
@@ -2,7 +2,9 @@ package com.team.seven.gocomix.model
 
 data class Comix(
     val author: Author,
+    @Deprecated("use `Comix.coverId` and method `image/{id}`")
     val cover: Image,
+    val coverId: Int = 1,
     val description: String,
     val id: Int,
     val rating: Float,
