ed7d94e1200c3e2aab6bd0e893cf923d0b2cc551
paveldolgov4 <dolgoff1272@gmail.com>
2022-05-20 23:37:47 +0300

    translate recognize useCase

--- app/src/main/java/com/team/seven/gocomix/domaim/interactor/TranslateRecognizedUseCaseImpl.kt
+++ app/src/main/java/com/team/seven/gocomix/domaim/interactor/TranslateRecognizedUseCaseImpl.kt
@@ -2,10 +2,37 @@ package com.team.seven.gocomix.domaim.interactor
 
 import android.graphics.Bitmap
 import com.team.seven.gocomix.domaim.Result
+import com.team.seven.gocomix.domaim.service.RecognizeService
+import com.team.seven.gocomix.domaim.service.TranslateService
 
-class TranslateRecognizedUseCaseImpl() : TranslateRecognizedUseCase {
+class TranslateRecognizedUseCaseImpl(
+    private val recognizer: RecognizeService,
+    private val translator: TranslateService
+) : TranslateRecognizedUseCase {
 
     override suspend fun execute(image: Bitmap): Result<List<String>> {
-        TODO("Not yet implemented")
+        return when (val state = recognizer.recognize(image)) {
+            is Result.Success -> {
+                translateBlocks(state.result)
+            }
+            is Result.Failure -> {
+                state
+            }
+        }
+    }
+
+    private suspend fun translateBlocks(blocks: List<String>): Result<List<String>> {
+        val translatedBlocks = arrayListOf<String>()
+        blocks.forEach {
+            when (val translateState = translator.translateIntoRussian(it)) {
+                is Result.Success -> {
+                    translatedBlocks.add(translateState.result)
+                }
+                is Result.Failure -> {
+                    return translateState
+                }
+            }
+        }
+        return Result.Success(translatedBlocks)
     }
 }
