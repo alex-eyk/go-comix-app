5891c80ca297c5e4e983a6792d665eaa3384a7ae
paveldolgov4 <dolgoff1272@gmail.com>
2022-05-20 23:37:47 +0300

    translate recognize useCase

--- app/src/main/java/com/team/seven/gocomix/domaim/Result.kt
+++ app/src/main/java/com/team/seven/gocomix/domaim/Result.kt
@@ -2,7 +2,6 @@ package com.team.seven.gocomix.domaim
 
 sealed class Result<out T : Any> {
 
-    object Loading : Result<Nothing>()
     data class Success<out T : Any>(val result: T) : Result<T>()
     data class Failure(val e: Exception) : Result<Nothing>()
 }
