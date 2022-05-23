c41418e6a716ac2ed20256156a42099abb69da55
Alexey Kiselev <ximand931@gmail.com>
2022-05-23 16:43:04 +0300

    ui state

---
+++ app/src/main/java/com/team/seven/gocomix/ui/UiState.kt
@@ -0,0 +1,20 @@
+package com.team.seven.gocomix.ui
+
+import com.team.seven.gocomix.util.Either
+
+sealed class UiState<out T : Any> {
+
+    object None: UiState<Nothing>()
+    object Loading : UiState<Nothing>()
+    data class Success<out T : Any>(val value: T) : UiState<T>()
+    data class Failure(val e: Exception) : UiState<Nothing>()
+
+    companion object {
+        fun <T : Any> by(result: Either<T>): UiState<T> {
+            return when (result) {
+                is Either.Success -> Success(result.value)
+                is Either.Failure -> Failure(result.e)
+            }
+        }
+    }
+}
