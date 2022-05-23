1248d7d499fa52c20d4f2642eccee9428bb464f0
Alexey Kiselev <ximand931@gmail.com>
2022-05-23 16:43:04 +0300

    ui state

--- app/src/main/java/com/team/seven/gocomix/util/Result.kt
+++
@@ -1,7 +0,0 @@
-package com.team.seven.gocomix.util
-
-sealed class Result<out T : Any> {
-
-    data class Success<out T : Any>(val result: T) : Result<T>()
-    data class Failure(val e: Exception) : Result<Nothing>()
-}
