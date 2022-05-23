d05241834b88c28d85218fa5b5c4a7329f4dc91e
Alexey Kiselev <ximand931@gmail.com>
2022-05-23 23:30:28 +0300

    empty either, image local repo with either

--- app/src/main/java/com/team/seven/gocomix/data/repo/image/ImageLocalRepository.kt
+++ app/src/main/java/com/team/seven/gocomix/data/repo/image/ImageLocalRepository.kt
@@ -1,19 +1,14 @@
 package com.team.seven.gocomix.data.repo.image
 
 import android.graphics.Bitmap
+import com.team.seven.gocomix.util.Either
+import com.team.seven.gocomix.util.EmptyEither
 
 interface ImageLocalRepository {
 
-    suspend fun getImage(id: String): ImageResult
+    suspend fun getImage(id: String): Either<Bitmap>
 
-    suspend fun saveImage(id: String, image: Bitmap): SaveResult
+    suspend fun saveImage(id: String, image: Bitmap): EmptyEither
 
     fun exists(id: String): Boolean
-}
-
-sealed class SaveResult {
-
-    object Loading : SaveResult()
-    object Success : SaveResult()
-    data class Failure(val error: Throwable) : SaveResult()
 }
