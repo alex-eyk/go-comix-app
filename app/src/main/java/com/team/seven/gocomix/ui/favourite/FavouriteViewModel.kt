6191d070605bbcd40c1f30eaeb6693835fc41540
Alexey Kiselev <ximand931@gmail.com>
2022-05-23 16:58:41 +0300

    remove old files

--- app/src/main/java/com/team/seven/gocomix/ui/favourite/FavouriteViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.team.seven.gocomix.ui.favourite
-
-import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
-import javax.inject.Inject
-
-@HiltViewModel
-class FavouriteViewModel @Inject constructor() : ViewModel()
